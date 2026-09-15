a828d7e6c1195bbf3ba7d48fb8080f05db3a9825
ghaziabbas309-byte <ghaziabbas309@gmail.com>
2026-09-14 17:46:55 -0700

    Completed lab1

---
+++ lab1.py
@@ -0,0 +1,12 @@
+# What is your full name
+print("Ghazi Abbas")
+
+# The area of a rectangle is length * width.
+# What is the area of a rectangle with a length of 10 and width of 7?
+print(10 * 7)
+
+# Fix the line below. Interpreters scan your entire file ahead-of-time to check for errors, unlike compilers.
+print("True")
+
+# Add one more color to the end of the list, after blue. (Don't forget the comma , and quotes "")
+print(["red", "orange", "blue", "green"])
