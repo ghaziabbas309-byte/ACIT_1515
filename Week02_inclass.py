03fb887bd43d9a5e95209a49392d3bb22334ec97
ghaziabbas309-byte <ghaziabbas309@gmail.com>
2026-09-17 16:04:29 -0700

    Add Week 02 in-class exercises

---
+++ Week02_inclass.py
@@ -0,0 +1,36 @@
+# Create a variable named x and store an integer (whole number) inside it
+
+# Create a variable named y and store a string (any characters between single or double quotes) inside it
+
+# Create a variable named z and store a float in it
+
+x = 5
+y = ("bcit is a good school")
+z = float(2)
+
+
+# Change the value stored in the x variable to a new string
+
+# Change the value stored in the y variable to a new boolean
+
+# Change the value store in the z variable to a different float
+
+x = ("i am a student at bcit")
+y = True
+z = float(9)
+
+# Print the (current) value of the variable x
+
+# Print the *type* (not the value itself) of the value stored in the y variable
+
+print(x)
+print(type(y))
+
+# Create two variables, one containing the string CIT, and another containing the string 1515
+
+# Using the two variables and a hard-coded letter, print the word ACIT1515 to the terminal
+
+
+Course = "CIT"
+Title = "1515"
+print("A" + Course + Title)
